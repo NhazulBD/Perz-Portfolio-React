761ccab7d20fb2f0da7d1372c6632ae1181aa2e5
Pedro Enrique Ramos Zafra <pedrinchi1999@gmail.com>
2022-01-30 10:35:21 -0400

    Base V3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/reset.css';
-import './styles/style.scss'
+import './styles/boots.scss';
+import './styles/style.scss';
 import './assets/fonts/fonts.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import './home.scss';
 
 export default function Home(state) {
 
   return (
     <>
       <section className={state.state === 0 ? "section home show":"section home hide"}>
-        
+        <div className="center-container">
+          <div className="center-block">
+            <div className="upper-content">
+              <div className="container contents">
+
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
