d0a831d721904aa88b1a8b757d5e163c92d8958f
Pedro Enrique Ramos Zafra <pedrinchi1999@gmail.com>
2022-01-30 11:09:09 -0400

    Page Bases

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -6,7 +6,16 @@ export default function Skills(state) {
   return (
     <>
       <section className={state.state === 2 ? "section show":"section hide"}>
-        
+        <div className="center-container">
+          <div className="center-block">
+            <div className="upper-content">
+              <div className="container contents">
+                <div className="row">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -6,7 +6,16 @@ export default function Works(state) {
   return (
     <>
       <section className={state.state === 3 ? "section show":"section hide"}>
-        
+        <div className="center-container">
+          <div className="center-block">
+            <div className="upper-content">
+              <div className="container contents">
+                <div className="row">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
