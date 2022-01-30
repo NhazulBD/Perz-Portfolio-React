52b558c9364804dfa29e76ab51f3255985f08d14
Pedro Enrique Ramos Zafra <pedrinchi1999@gmail.com>
2022-01-30 00:15:09 -0400

    Borders V2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,20 +7,39 @@ export default function Home() {
   const [zoomIn, setZoomIt] = useState(false);
   const [menuT, setMenuT] = useState(false);
   const [activeItem, setActiveItem] = useState(0);
+  const [secondary, setSecondary] = useState(false);
 
   return (
     <>
       {/* borders */}
-      <div className="the-borders border-top"></div>
-      <div className="the-borders border-right"></div>
-      <div className="the-borders border-bottom"></div>
-      <div className="the-borders border-left"></div>
+      <div className={secondary ? "the-borders border-top secondary":"the-borders border-top"}></div>
+      <div className={secondary ? "the-borders border-right secondary":"the-borders border-right"}></div>
+      <div className={secondary ? "the-borders border-bottom secondary":"the-borders border-bottom"}></div>
+      <div className={secondary ? "the-borders border-left secondary":"the-borders border-left"}></div>
       {/* waves */}
-      <canvas class="border-wave" id="wave"></canvas>
+      <div style={{height:"665px",width:"1319px", position:"relative"}}>
+        <svg className={secondary ? "border-wave secondary":"border-wave"} xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none">
+          <defs>
+            <linearGradient id="wave-gradient" gradientTransform="rotate(90)">
+              <stop offset="5%" stopColor="#ffffff" />
+              <stop offset="35%" stopColor="#ffffff" />
+            </linearGradient>
+            <path id="a" d="M-160 44c30 0 58-18 88-18s58 18 88 18 58-18 88-18 58 18 88 18v44h-352z" fill="#ffffff" />
+          </defs>
+          <g className="wave-paths">
+            <use href="#a" x="0" />
+            <use href="#a" x="50" y="3" />
+            <use href="#a" x="100" y="5" />
+            <use href="#a" x="150" y="7" />
+            <use href="#a" x="200" y="7" />
+          </g>
+        </svg>
+      </div>
+      {/* <canvas className={secondary ? "border-wave secondary":"border-wave"}></canvas> */}
       {/* logo */}
-      <div className="logo"></div>
+      <div className={secondary ? "logo secondary":"logo"}></div>
       {/* button menu */}
-      <div className="menu-trigger" onClick={()=>{if(menuT){setMenuT(false);}else{setMenuT(true);}}}>
+      <div className={secondary ? "menu-trigger secondary":"menu-trigger"} onClick={()=>{if(menuT){setMenuT(false);}else{setMenuT(true);}}}>
         <button className={menuT ? "lines-button menu-toggle":"lines-button menu-toggle open"}>
           <span className="lines"></span>
         </button>
@@ -34,6 +53,7 @@ export default function Home() {
                 <p onClick={()=>{
                   setMenuT(false);
                   setZoomIt(false);
+                  setSecondary(false);
                   setActiveItem(0);
                   }}><span className="cross-out">Inicio</span></p>
               </li>
@@ -41,6 +61,7 @@ export default function Home() {
                 <p onClick={()=>{
                   setMenuT(false);
                   setZoomIt(true);
+                  setSecondary(true);
                   setActiveItem(1);
                 }}><span className="cross-out">Acerca de mí</span></p>
               </li>
@@ -48,6 +69,7 @@ export default function Home() {
                 <p onClick={()=>{
                   setMenuT(false);
                   setZoomIt(true);
+                  setSecondary(true);
                   setActiveItem(2);
                 }}><span className="cross-out">Habilidades</span></p>
               </li>
@@ -55,6 +77,7 @@ export default function Home() {
                 <p onClick={()=>{
                   setMenuT(false);
                   setZoomIt(true);
+                  setSecondary(true);
                   setActiveItem(3);
                 }}><span className="cross-out">Trabajos</span></p>
               </li>
