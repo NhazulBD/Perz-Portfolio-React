4e8a74e54f17f90333ac84ad23269a7e667d9c89
Pedro Enrique Ramos Zafra <pedrinchi1999@gmail.com>
2022-01-30 12:41:46 -0400

    Titles V2

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import './works.scss';
+import TitlesH2 from "../../components/titles/Titles";
 
 export default function Works(state) {
   return (
     <>
-      <section className={state.state === 3 ? "section show":"section hide"}>
+      <section className={state.state === 3 ? "section works show":"section works hide"}>
         <div className="center-container">
           <div className="center-block">
             <div className="lower-content">
               <div className="container contents">
                 <div className="row">
+                  <div className="col col-12">
+                    <TitlesH2 subtitle="Lo que he hecho" title="Trabajos" />
+                  </div>
                 </div>
               </div>
             </div>
