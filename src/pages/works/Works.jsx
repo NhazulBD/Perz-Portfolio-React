f0895f8ed0634be3be4e6c96214907b6a991ee76
Pedro Enrique Ramos Zafra <pedrinchi1999@gmail.com>
2022-01-31 12:17:11 -0400

    Works base

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -4,6 +4,9 @@ import SectionContentLayout from "../../layouts/sectioncontentlayout/SectionCont
 import TitlesH2 from "../../components/titles/Titles";
 
 export default function Works(state) {
+
+  const [toggleSection, setToggleSection] = useState(false);
+
   return (
     <section className={state.state === 3 ? "section works show":"section works hide"}>
       <SectionContentLayout>
@@ -12,6 +15,33 @@ export default function Works(state) {
             <TitlesH2 subtitle="Lo que he hecho" title="Trabajos" />
           </div>
         </div>
+        <div className="divider-ot"></div>
+        <div className="row">
+          <div className="col-12 text-center">
+            <span 
+              className={!toggleSection ? "btn active fullwidth-liquid mx-1":"btn fullwidth-liquid mx-1"}
+              onClick={() => setToggleSection(false)}
+            >
+              <span>Branding</span>
+            </span>
+            <span 
+              className={toggleSection ? "btn active fullwidth-liquid mx-1":"btn fullwidth-liquid mx-1"}
+              onClick={() => setToggleSection(true)}
+            >
+              <span>Web</span>
+            </span>
+          </div>
+        </div>
+        <div className="divider-ot"></div>
+        {!toggleSection ? (
+          <>
+            Branding
+          </>
+        ):(
+          <>
+            Web
+          </>
+        )}
       </SectionContentLayout>
     </section>
   );
